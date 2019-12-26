0d7f2b15e6ae89a9f671b350940c7a4f144443ca
Nathan Hall <nrh20@pitt.edu>
2019-12-26 09:33:36 -0500

    Updates

---
+++ precache-manifest.9ed20b222f437461c42cd16cc0762e9b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e596fdfc8a7fc281c9bf11c2a461e9da",
+    "url": "/PersonalWebsite/index.html"
+  },
+  {
+    "revision": "6f4bb9336cff50e6348c",
+    "url": "/PersonalWebsite/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b87321441f84a4534f27",
+    "url": "/PersonalWebsite/static/css/main.329d124c.chunk.css"
+  },
+  {
+    "revision": "6f4bb9336cff50e6348c",
+    "url": "/PersonalWebsite/static/js/2.a5c59fce.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/PersonalWebsite/static/js/2.a5c59fce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b87321441f84a4534f27",
+    "url": "/PersonalWebsite/static/js/main.3bc6cc79.chunk.js"
+  },
+  {
+    "revision": "84f2361143ec72823efb",
+    "url": "/PersonalWebsite/static/js/runtime-main.6c761c50.js"
+  },
+  {
+    "revision": "6d745c14379c6916449b5f2f35bf414a",
+    "url": "/PersonalWebsite/static/media/ASM.6d745c14.png"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/PersonalWebsite/static/media/Cpp.f278be79.png"
+  },
+  {
+    "revision": "e068a7fe43fbf17c16de64f26c5e7ccc",
+    "url": "/PersonalWebsite/static/media/JS.e068a7fe.png"
+  },
+  {
+    "revision": "d9fa5fbfc369d4019d9a9d9ac40f6b5d",
+    "url": "/PersonalWebsite/static/media/JavaImg.d9fa5fbf.png"
+  },
+  {
+    "revision": "040321e6f15479cad43c7c33a6161c8d",
+    "url": "/PersonalWebsite/static/media/NRHResume.040321e6.png"
+  },
+  {
+    "revision": "b76ac5162502777604bfbc00807b0ff4",
+    "url": "/PersonalWebsite/static/media/ProfilePic.b76ac516.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PersonalWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WebsiteRepo/precache-manifest.21956adcd9bf0d389be86dd273f955ae.js"
+  "/PersonalWebsite/precache-manifest.9ed20b222f437461c42cd16cc0762e9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WebsiteRepo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PersonalWebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

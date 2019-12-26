f636596c236b0b8665ac1aa3b4316e220d0dc219
Nathan Hall <nrh20@pitt.edu>
2019-12-26 09:42:44 -0500

    Updates

--- precache-manifest.7c3a4c3f7cf72a77d2c2627b9871314e.js
+++ precache-manifest.7c3a4c3f7cf72a77d2c2627b9871314e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e766b2432b29439a23e52b745d64a430",
+    "revision": "8c3007435971e727729cb26bb5f973a1",
     "url": "/PersonalWebsite/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/PersonalWebsite/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "289f3b1a410bdd89cdca",
+    "revision": "d5561c40b69ebe9a3f07",
     "url": "/PersonalWebsite/static/css/main.329d124c.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/PersonalWebsite/static/js/2.a5c59fce.chunk.js.LICENSE"
   },
   {
-    "revision": "289f3b1a410bdd89cdca",
-    "url": "/PersonalWebsite/static/js/main.fa2fa6ab.chunk.js"
+    "revision": "d5561c40b69ebe9a3f07",
+    "url": "/PersonalWebsite/static/js/main.6ea012b3.chunk.js"
   },
   {
     "revision": "84f2361143ec72823efb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PersonalWebsite/precache-manifest.99f17f5293a286c42772ae3de428840d.js"
+  "/PersonalWebsite/precache-manifest.7c3a4c3f7cf72a77d2c2627b9871314e.js"
 );
 
 self.addEventListener('message', (event) => {
